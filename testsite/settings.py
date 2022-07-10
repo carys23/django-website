6aa056af441372e7b93c2c9aacd574c89d252d09
carys <caryspritch@gmail.com>
2022-07-10 12:38:50 +0100

    updated

--- testsite/settings.py
+++ testsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@@g9m&p&5qh2qk53y)afil2mdw5laqdu%!)92me-1mdo%4ics_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
